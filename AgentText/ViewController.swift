863d2eb17ca9b18d0f5a9fe7611fa0cb5195fa97
Max Xiao <maxxiao@umich.edu>
2025-11-26 00:55:22 -0500

    Initial Commit

---
+++ AgentText/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AgentText
+//
+//  Created by Max Xiao on 11/26/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
